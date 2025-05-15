9418210672cb9b18ed1ff46145e52cd655808e1b
Md Maruf Prodhan <59418855+mdmarufprodhan@users.noreply.github.com>
2025-05-15 14:24:08 +0600

    added

--- MarufPortfolioApp/Models/Project.swift
+++ MarufPortfolioApp/Models/Project.swift
@@ -1,8 +1,14 @@
 //
-//  Project.swift
+//  Project 2.swift
 //  MarufPortfolioApp
 //
 //  Created by Maruf on 5/15/25.
 //
-
 import Foundation
+
+struct Project: Identifiable {
+    let id = UUID()
+    let title: String
+    let description: String
+    let url: String
+}
