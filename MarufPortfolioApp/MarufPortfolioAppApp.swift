e00c3926a76841145a4ed9d5f63edbf2f049cf89
Md Maruf Prodhan <mdmaruf.shadhin@gmail.com>
2025-05-15 13:18:29 +0600

    Initial Commit

---
+++ MarufPortfolioApp/MarufPortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarufPortfolioAppApp.swift
+//  MarufPortfolioApp
+//
+//  Created by Maruf on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MarufPortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
